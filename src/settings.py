effb086b7e866b9b887b4abba3fbf473fc5c2e0a
stuartelimu <stuartelimu@gmail.com>
2021-07-13 15:21:15 +0300

    update settings

--- src/settings.py
+++ src/settings.py
@@ -127,11 +127,11 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
-STATIC_ROOT =  BASE_DIR / "static_root"
+# STATIC_ROOT =  BASE_DIR / "static_root"
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
